7e01cb0f60e030d119bcae55f6913da38f796df0
alumne <joanchorto1@iesebre.com>
2024-09-30 18:46:12 +0200

    first commit

---
+++ src/views/BlogView.vue
@@ -0,0 +1,102 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class=" flex flex-col min-h-screen items-center justify-center space-y-10 pb-20 text-gray-50 bg-gray-200">
+    <!--    header-->
+    <div class="w-full bg-gradient-to-r from-blue-700 to-indigo-900 flex space-x-5   " style="height: 35em">
+      <div class="flex flex-col  items-center justify-center text-center space-y-10 p-16 w-2/3">
+        <h1 class=" font-bold uppercase text-7xl">Nuestras herramientas</h1>
+        <h2 class="font-bold uppercase text-5xl">Mejoraras los procesos de tu empresa</h2>
+
+        <p class="text-lg text-gray-200">Softwar empresarial diseñado para mejorar tu rendimiento </p>
+      </div>
+      <div class="w-1/3 items-center justify-center flex-col flex">
+        <img src="/public/softw.jpeg" class="h-4/5 rounded-2xl shadow-gray-50 shadow-2xl" alt="">
+      </div>
+    </div>
+    <!--    Seccio CRM-->
+    <div class="flex flex-col p-10 m-24 space-y-10 rounded-2xl bg-gray-50  w-2/3">
+      <div class="flex">
+        <div class="w-1/6">
+          <img src="/public/CRM_Icon.jpeg" alt="" class="h-32 w-32  rounded-2xl">
+        </div>
+        <div class="w-5/6 flex flex-col">
+          <h3 class="text-2xl font-bold  text-blue-500">CRM</h3>
+          <p class="text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, animi aperiam asperiores cumque deleniti ea eveniet illum, iste iure laboriosam natus neque porro qui reiciendis repellat saepe tempora voluptas voluptatum.</p>
+        </div>
+      </div>
+      <div class="flex items-center justify-center  flex-col">
+        <button class=" w-32 px-4 py-2 bg-green-500 rounded-xl hover:bg-green-700 hover:font-bold"><a href="/products/CRM">Ver Ofertas</a></button>
+      </div>
+    </div>
+    <!--    Seccio Projects-->
+    <div class="flex flex-col p-10 m-24 space-y-10 rounded-2xl bg-gray-50  w-2/3">
+      <div class="flex">
+        <div class="w-1/6">
+          <img src="/public/Projects%20logo.jpeg" alt="" class="h-32 w-32  rounded-2xl">
+        </div>
+        <div class="w-5/6 flex flex-col">
+          <h3 class="text-2xl font-bold  text-blue-500">Software de gestion de proyectos</h3>
+          <p class="text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, animi aperiam asperiores cumque deleniti ea eveniet illum, iste iure laboriosam natus neque porro qui reiciendis repellat saepe tempora voluptas voluptatum.</p>
+        </div>
+      </div>
+      <div class="flex items-center justify-center  flex-col">
+        <button class=" w-32 px-4 py-2 bg-green-500 rounded-xl hover:bg-green-700 hover:font-bold"><a href="/products/Projects">Ver Ofertas</a></button>
+      </div>
+    </div>
+    <!--    Seccio Inventary-->
+    <div class="flex flex-col p-10 m-24 space-y-10 rounded-2xl bg-gray-50  w-2/3">
+      <div class="flex">
+        <div class="w-1/6">
+          <img src="/public/Inventary_Icon.jpeg" alt="" class="h-32 w-32  rounded-2xl">
+        </div>
+        <div class="w-5/6 flex flex-col">
+          <h3 class="text-2xl font-bold  text-blue-500">Software de gestion de inventarios</h3>
+          <p class="text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, animi aperiam asperiores cumque deleniti ea eveniet illum, iste iure laboriosam natus neque porro qui reiciendis repellat saepe tempora voluptas voluptatum.</p>
+        </div>
+      </div>
+      <div class="flex items-center justify-center  flex-col">
+        <button class=" w-32 px-4 py-2 bg-green-500 rounded-xl hover:bg-green-700 hover:font-bold"><a href="/products/Inventory">Ver Ofertas</a></button>
+      </div>
+    </div>
+    <!--    Seccio Gesti-->
+    <div class="flex flex-col p-10 m-24 space-y-10 rounded-2xl bg-gray-50  w-2/3">
+      <div class="flex">
+        <div class="w-1/6">
+          <img src="/public/POPLogo.jpeg" alt="" class="h-32 w-32  rounded-2xl">
+        </div>
+        <div class="w-5/6 flex flex-col">
+          <h3 class="text-2xl font-bold  text-blue-500">Software de gestion y facturacion</h3>
+          <p class="text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, animi aperiam asperiores cumque deleniti ea eveniet illum, iste iure laboriosam natus neque porro qui reiciendis repellat saepe tempora voluptas voluptatum.</p>
+        </div>
+      </div>
+      <div class="flex items-center justify-center  flex-col">
+        <button class=" w-32 px-4 py-2 bg-green-500 rounded-xl hover:bg-green-700 hover:font-bold"><a href="/products/GestiPOP">Ver Ofertas</a></button>
+      </div>
+    </div>
+    <!--    Seccio Talent-->
+    <div class="flex flex-col p-10 m-24 space-y-10 rounded-2xl bg-gray-50  w-2/3">
+      <div class="flex">
+        <div class="w-1/6">
+          <img src="/public/TalentIcon.jpeg" alt="" class="h-32 w-32  rounded-2xl">
+        </div>
+        <div class="w-5/6 flex flex-col">
+          <h3 class="text-2xl font-bold  text-blue-500">Software de gestion de talento y rendimiento </h3>
+          <p class="text-gray-400">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias, animi aperiam asperiores cumque deleniti ea eveniet illum, iste iure laboriosam natus neque porro qui reiciendis repellat saepe tempora voluptas voluptatum.</p>
+        </div>
+      </div>
+      <div class="flex items-center justify-center  flex-col">
+        <button class=" w-32 px-4 py-2 bg-green-500 rounded-xl hover:bg-green-700 hover:font-bold"><a href="/products/Talent&Rendiment">Ver Ofertas</a></button>
+      </div>
+    </div>
+
+
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
--- src/views/JctagencyView.vue
+++ src/views/JctagencyView.vue
@@ -1,26 +1,26 @@
 <template>
-<div class="flex flex-col justify-center min-h-screen bg-black space-y-40 items-center">
-  <div class="flex flex-col justify-center space-y-10 border-b-8 pb-24 border-gray-200 items-center">
-    <h2 class="text-7xl uppercase font-bold">Mejoramos constantemente!!</h2>
-    <p class="text-xl w-4/5 text-center ">Estamos reinventando nuestra empresa para ofrecerte el mejor producto para PTENCIAR i AGILIZAR al maximo tus tareas empresariales</p>
+<div class="flex flex-col bg-gray-50 justify-center min-h-screen  space-y-40 items-center">
+  <div class="flex flex-col justify-center space-y-10 border-b-8 border-gray-900 pb-24 border-gray-200 items-center">
+    <h2 class="text-7xl text-gray-500 uppercase font-bold">Mejoramos constantemente!!</h2>
+    <p class="text-xl text-gray-400 w-4/5 text-center ">Estamos reinventando nuestra empresa para ofrecerte el mejor producto para PTENCIAR i AGILIZAR al maximo tus tareas empresariales</p>
   </div>
   <div class="flex flex-col justify-center space-y-10 items-center">
-    <h2 class="text-5xl uppercase font-bold">Se acabo lo de estar tirando tu tiempo!</h2>
-    <p class=" text-lg">Si te interesa saber mas información sobre nosotros dejanos tu mail y nos pondremos en contacto contigo</p>
+    <h2 class="text-5xl text-gray-500 uppercase font-bold">Se acabo lo de estar tirando tu tiempo!</h2>
+    <p class=" text-lg text-gray-400">Si te interesa saber mas información sobre nosotros dejanos tu mail y nos pondremos en contacto contigo</p>
     <form class="flex-col w-full items-center justify-center flex" @submit.prevent="handleSubmit" >
       <div class="flex  space-x-10">
         <div class="flex flex-col space-y-6">
-          <label class="font-bold"  >Nombre</label>
-          <input class="rounded-xl text-gray-900" type="text" id="nombre" name="nombre" v-model="formFields.nombre"  required>
+          <label class="font-bold text-gray-500"  >Nombre</label>
+          <input class="bg-gray-900 rounded-xl text-gray-100" type="text" id="nombre" name="nombre" v-model="formFields.nombre"  required>
         </div>
         <div class="flex flex-col space-y-6">
-          <label class="font-bold" >Email</label>
-          <input class="rounded-xl text-gray-900" type="email" id="email"  name="email" v-model="formFields.email"  required>
+          <label class="font-bold text-gray-500" >Email</label>
+          <input class="bg-gray-900 rounded-xl text-gray-100" type="email" id="email"  name="email" v-model="formFields.email"  required>
         </div>
 
       </div>
       <div class="flex flex-col mt-10 items-center justify-center">
-        <button type="submit" class="bg-gray-200 text-gray-950 rounded-xl px-6 p-2">Enviar</button>
+        <button type="submit" class="bg-green-500 text-gray-50 rounded-xl px-6 p-2">Enviar</button>
 
       </div>
       <div v-if="isFormSubmitted">
